672ce00b116fba3b7359f0e888e433b3a4206965
Christopher Plain <me@christopherplain.com>
2023-12-02 11:44:39 -0800

    Refactor day 1 part 2

--- 2023/day01_02.py
+++ 2023/day01_02.py
@@ -1,13 +1,10 @@
 # Advent of Code 2023
 
-# Day 1: Trebuchet?!
-# Part Two
+# Day 1: Trebuchet?! (Part Two)
 # Combine fist and last digit of each line in doc and sum.
 # Digit could be spelled.
 
-# Test input
-# 29, 83, 13, 24, 42, 14, and 76 sum equals 281
-#
+# Test input:
 # two1nine
 # eightwothree
 # abcone2threexyz
@@ -15,29 +12,33 @@
 # 4nineeightseven2
 # zoneight234
 # 7pqrstsixteen
+#
+# Test answer:
+# 29 + 83 + 13 + 24 + 42 + 14 + 76 = 281
 
 
 def sum_calibration(doc):
     numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
     sum = 0
+
     with open(doc) as f:
         lines = f.readlines()
+
         for line in lines:
-            first_num, last_num = "", ""
+            first_num, last_num = 0, 0
             first_index, last_index = float("inf"), float("-inf")
 
-            digits = "".join(filter(str.isdigit, line))
-            if len(digits) > 0:
-                first_num, last_num = digits[0], digits[len(digits) - 1]
-                first_index = line.index(first_num)
-                last_index = line.rindex(last_num)
-
             for i, num in enumerate(numbers, start=1):
-                if num not in line:
-                    continue
+                l_index, r_index = float("inf"), float("-inf")
+                i_str = str(i)
 
-                l_index = line.index(num)
-                r_index = line.rindex(num)
+                if i_str in line:
+                    l_index = line.index(i_str)
+                    r_index = line.rindex(i_str)
+
+                if num in line:
+                    l_index = min(l_index, line.index(num))
+                    r_index = max(r_index, line.rindex(num))
 
                 if l_index < first_index:
                     first_num = i
@@ -48,6 +49,7 @@ def sum_calibration(doc):
                     last_index = r_index
 
             sum += int(first_num) * 10 + int(last_num)
+
     return sum
 
 
